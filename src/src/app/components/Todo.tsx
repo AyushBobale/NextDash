59fbf427de6f1044e456da8aecd83f3d582dcbe8
Ayush Bobale <bobaleayush@gmail.com>
2023-07-19 10:56:41 +0530

    Todo done

--- src/src/app/components/Todo.tsx
+++ src/src/app/components/Todo.tsx
@@ -1,13 +1,46 @@
+import Attachment from "../../../public/static/images/attachment.svg";
 import Image from "next/image";
+import TaskList from "../../../public/static/images/list.svg";
+import Time from "../../../public/static/images/access_alarms.svg";
 import styles from "../components/css/Todo.module.css";
 
-const Todo = ({ image, taskCategory, taskName }) => {
+const Todo = ({ image, taskCategory, taskName, profiles, duration }) => {
   return (
     <div className={styles["todo-wrap"]}>
       {image && <Image src={image} alt="" />}
       <div className={styles["todo-inner"]}>
-        <div className={styles["profile-cont"]}>{taskCategory}</div>
+        <div className={styles["profile-cont"]}>
+          {taskCategory}
+          <div className={styles["profile-imgs"]}>
+            {profiles?.map((elm, idx) => {
+              return (
+                <Image
+                  src={elm}
+                  alt=""
+                  style={{
+                    transform: `translateX(${idx * -80}%)`,
+                    zIndex: `${profiles?.length + -1 * idx}`,
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
         <span className={styles["task-name"]}>{taskName}</span>
+        {duration && (
+          <div className={styles["todo-btm-row"]}>
+            <span>
+              <Image src={TaskList} alt="" /> {duration?.tasks}
+            </span>
+            <span>
+              <Image src={Attachment} alt="" /> {duration?.attachments}
+            </span>
+            <span>
+              <Image src={Time} alt="" />
+              {duration?.daysLeft} Days left
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/src/app/components/TodoCol.tsx
+++ src/src/app/components/TodoCol.tsx
@@ -1,4 +1,8 @@
 import Add from "../../../public/static/images/add.svg";
+import Avtaar1 from "../../../public/static/images/Avatar Image.png";
+import Avtaar2 from "../../../public/static/images/Avatar Image(1).png";
+import Avtaar3 from "../../../public/static/images/Avatar Image(2).png";
+import Avtaar4 from "../../../public/static/images/Avatar Image(3).png";
 import Image from "next/image";
 import Task1 from "../../../public/static/images/task1.png";
 import ThreeDotsH from "../../../public/static/images/h-3-dots.svg";
@@ -31,6 +35,8 @@ const TodoCol = ({ title, color, todos }) => {
               image={Task1}
               taskCategory={"Space Task 2"}
               taskName={"Make money online"}
+              profiles={[Avtaar1, Avtaar2, Avtaar3]}
+              duration={{ tasks: 4, attachments: 2, daysLeft: 6 }}
             />
           );
         })}
